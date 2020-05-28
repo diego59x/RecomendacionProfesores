34b3a1d826f3c30a43bc27c37ec8c0e6c566f98e
Reyna31 <53918885+Reyna31@users.noreply.github.com>
2020-05-28 00:11:24 -0600

    Add files via upload

--- Defensiva.py
+++ Defensiva.py
@@ -1,7 +1,9 @@
-# Pablo Reyna
-
-
-
+# Alejandro Corzo  
+# Paulina Perez    
+# El otro     
+# El otro x2  
+# FECHA : 20/10/2019
+# SECCION
 # Funciones de defensa
 def Entero(opcion):
     # se verifica si se puede convertir a entero lo que ingresa el usuario
